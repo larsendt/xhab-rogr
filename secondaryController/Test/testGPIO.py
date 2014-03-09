aa3a6843fa447592031994834c8970057f2a0e3b
inganesa <anitha.ganesha@colorado.edu>
2014-03-09 01:26:03 -0700

    test code for setting GPIO config

---
+++ secondaryController/Test/testGPIO.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from ctypes import *
+helper = cdll.LoadLibrary("./libsub.so")
+dev_id = helper.sub_open(0)
+INTP = POINTER(c_int)
+num = c_int(0)
+addr = addressof(num)
+ptr = cast(addr, INTP)
+rval = helper.sub_gpio_config(dev_id, 0x80, ptr,0xFF)
+print ptr[0]
+print rval
+helper.sub_close(dev_id)
