51f49836c9566953b10b87b1fa1d45171425a3dd
Anitha Upadhyaya <ani@ani-Inspiron-5423.(none)>
2014-03-14 16:14:00 -0600

    adding first python wrapper function prototype for subopen,close,and lcdwrite

--- secondaryController/sub20Module/sub20.py
+++ secondaryController/sub20Module/sub20.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 from ctypes import *
 from ctypes.util import find_library
 import os.path
@@ -15,9 +14,31 @@ subfile = os.path.basename(liblocation)
 #pointer to actual library file
 sublib = numpy.ctypeslib.load_library(subfile,subpath)
 
+#*_raw are python objects that points to corresponding c functions in the c library.
+sub_open_raw = sublib.sub_open
+sub_lcd_write_raw = sublib.sub_lcd_write
+sub_close_raw = sublib.sub_close
 
-#helper = cdll.LoadLibrary("./libsub.so")
-dev_id = sublib.sub_open(0)
-sublib.sub_lcd_write(dev_id, "\fAwesome\nworld")
-sublib.sub_close(dev_id)
-print dev_id
+
+#python function argument types
+sub_open_raw.argtypes = [c_int]
+sub_lcd_write_raw.argtypes = [c_int,c_char_p]
+sub_close_raw.argtypes = [c_int]
+
+#python function wrapper definitions
+def sub_open(hndl):
+    devid = c_int()
+    devid = sub_open_raw(hndl)
+    return(devid)
+    
+def sub_lcd_write(devid, string):
+    rt = c_int()
+    rt = sub_lcd_write_raw(devid,string)
+    return(rt)
+
+def sub_close(devid):
+    rt = c_int()
+    rt = sub_close_raw(devid,byref(rt))
+    return(rt)
+
+
