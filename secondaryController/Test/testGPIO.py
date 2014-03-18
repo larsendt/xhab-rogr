50605f0f4c95664e1b4c3278c7b5bc5180a85bb8
inganesa <anitha.ganesha@colorado.edu>
2014-03-17 23:04:43 -0600

    adding library folder

--- secondaryController/Test/testGPIO.py
+++ secondaryController/Test/testGPIO.py
@@ -7,7 +7,7 @@ INTP = POINTER(c_int)
 num = c_int(0)
 addr = addressof(num)
 ptr = cast(addr, INTP)
-rval = helper.sub_gpio_config(dev_id, 0x80, ptr,0xFF)
-print ptr[0]
+rval = helper.sub_gpio_config(dev_id, 0x80, addr ,0xFF)
+print num
 print rval
 helper.sub_close(dev_id)
