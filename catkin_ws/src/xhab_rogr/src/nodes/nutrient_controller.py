b4c1386813320a5a9393a10f4074b76a598dc5df
Dane Larsen <dane.t.larsen@gmail.com>
2014-04-05 16:04:46 -0600

    added nodes for the nutrient and water pumps

---
+++ catkin_ws/src/xhab_rogr/src/nodes/nutrient_controller.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import rospy
+from xhab_rogr.msg import *
+
+class NutrientController(object):
+    def __init__(self):
+        rospy.init_node("NutrientController")
+        subtopic = "/tasks/rogr/nutrient"
+        self.sub = rospy.Subscriber(subtopic, NutrientPumpTask, self.callback)
+
+    def callback(self, msg):
+        if msg.pump_on:
+            print "pump on"
+        else:
+            print "pump off"
+
+    def spin(self):
+        rospy.spin()
+
+
+if __name__ == "__main__":
+    c = NutrientController()
+    c.spin()
