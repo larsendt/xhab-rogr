01fa9adcf75f53a5a1a7d6b81c8de659b049866e
ROGR <dane.t.larsen@gmail.com>
2014-04-10 09:58:40 -0600

    video stream to html

---
+++ camera/server.py
@@ -0,0 +1,34 @@
+from geventwebsocket.handler import WebSocketHandler
+from gevent.pywsgi import WSGIServer
+from flask import Flask, request
+from time import sleep
+from cStringIO import StringIO
+
+import pygame
+pygame.init()
+
+import SimpleCV as scv
+
+app = Flask(__name__)
+cam = scv.Camera(0)
+
+
+@app.route('/camera')
+def camera():
+
+    if request.environ.get('wsgi.websocket'):
+        ws = request.environ['wsgi.websocket']
+
+        while True:
+            fp = StringIO()
+            image = cam.getImage().flipHorizontal().getPIL()
+            image.save(fp, 'JPEG')
+            ws.send(fp.getvalue().encode("base64"))
+            #fp.close() << benchmark and memory tests needed
+            sleep(0.05)
+
+
+if __name__ == '__main__':
+    #put ROGR's ip address here
+    http_server = WSGIServer(("10.203.75.78",8080), app, handler_class=WebSocketHandler)
+    http_server.serve_forever()
