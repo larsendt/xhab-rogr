7fa202723f70c4dda7f42c86c11031999b0212b9
Anitha Ganesha <anitha.ganesha@colorado.edu>
2014-04-12 15:40:25 -0600

    Update muxconfig.py

--- secondaryController/sensor_nodes/muxconfig.py
+++ secondaryController/sensor_nodes/muxconfig.py
@@ -33,7 +33,7 @@ channelToValue = { 0: 0x00000000,
 muxmask = setpin(GPIO_MUX_ENABLE_PIN) | setpin(GPIO_MUX_S0_PIN) | setpin(GPIO_MUX_S1_PIN) | setpin(GPIO_MUX_S2_PIN)
 
 def getSelectValue(pin):
-  val = channelToValue[pin]()
+  val = channelToValue[pin]
   return(val)
 
 
