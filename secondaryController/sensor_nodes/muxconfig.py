a2857cc6400318d36ea24917c49ac96da2c5d7fe
Anitha Ganesha <anitha.ganesha@colorado.edu>
2014-04-12 14:49:34 -0600

    Update muxconfig.py

--- secondaryController/sensor_nodes/muxconfig.py
+++ secondaryController/sensor_nodes/muxconfig.py
@@ -11,13 +11,13 @@ def setpin(output,pin):
   return(output)
 
 channelToValue = { 0: 0x00000000,
-            1: setgiopin(GPIO_MUX_S0_PIN),
-            2: setgpiopin(GPIO_MUX_S1_PIN),
-            3: setgpiopin(GPIO_MUX_S0_PIN)| setgpiopin(GPIO_MUX_S1_PIN),
-            4: setgpiopin(GPIO_MUX_S2_PIN),
-            5: setgpiopin(GPIO_MUX_S0_PIN) | setgpiopin(GPIO_MUX_S2_PIN),
-            6: setgpiopin(GPIO_MUX_S1_PIN) | setgpiopin(GPIO_MUX_S2_PIN),
-            7: setgpiopin(GPIO_MUX_S0_PIN) | setgpiopin(GPIO_MUX_S1_PIN) | setgpiopin(GPIO_MUX_S2_PIN),
+            1: setpin(GPIO_MUX_S0_PIN),
+            2: setpin(GPIO_MUX_S1_PIN),
+            3: setpin(GPIO_MUX_S0_PIN)| setpin(GPIO_MUX_S1_PIN),
+            4: setpin(GPIO_MUX_S2_PIN),
+            5: setpin(GPIO_MUX_S0_PIN) | setpin(GPIO_MUX_S2_PIN),
+            6: setpin(GPIO_MUX_S1_PIN) | setpin(GPIO_MUX_S2_PIN),
+            7: setpin(GPIO_MUX_S0_PIN) | setpin(GPIO_MUX_S1_PIN) | setpin(GPIO_MUX_S2_PIN),
             }
             
 muxmask = setpin(GPIO_MUX_ENABLE_PIN) | setpin(GPIO_MUX_S0_PIN) | setpin(GPIO_MUX_S1_PIN) | setpin(GPIO_MUX_S2_PIN)
