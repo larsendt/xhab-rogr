27c21ba4c86fe1464ac20323c258d1363067ef55
Anitha Ganesha <anitha.ganesha@colorado.edu>
2014-04-12 18:12:09 -0600

    Update pressureSensor.py

--- secondaryController/sensor_nodes/pressureSensor.py
+++ secondaryController/sensor_nodes/pressureSensor.py
@@ -8,17 +8,19 @@ LEFT = 0
 RIGHT = 1
 
 def getPressureLevel(direction):
-  #open device 
+  #open device 	
   devid = sub_open(0)
   #enable ADC module
   sub_adc_config(devid, 0x8040)#enable ADC with VCC ref
   #enable mux GPIOPins as output mode
   rt,status = sub_gpio_config(devid, muxmask, muxmask) # mask and set are the same for ouput mode
   #set PRESSURESENSOR select line
-  if(direction  == LEFT)
+  if(direction  == LEFT):
     selectval = getSelectValue(MUX_LEFT_PRESSURE_SENSOR_PIN)
-  else
+  elif:
     selectval = getSelectValue(MUX_RIGHT_PRESSURE_SENSOR_PIN)
+  else:
+  	return(-1)
   rt,status = sub_gpio_write(devid,selectval,muxmask)
   #read READPRESSURELEVEL 
   rt,level = sub_adc_single(devid,ADC_MUX_SIGNAL_PIN)
