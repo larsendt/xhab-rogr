4f0561c9137faf98db6f465fc34d3215ebaa2025
Dane Larsen <dane.t.larsen@gmail.com>
2014-04-15 19:27:43 -0600

    gamepad analog sticks don't always go completely back to zero, so do a threshold thing

--- catkin_ws/src/xhab_rogr/src/nodes/gamepad_control.py
+++ catkin_ws/src/xhab_rogr/src/nodes/gamepad_control.py
@@ -13,25 +13,27 @@ class GamepadControl(object):
         self.pub = rospy.Publisher(pubtopic, DrivingTask)
 
     def spin(self):
-        last_x_move = None
-        last_y_move = None
-        last_rot = None
         while not rospy.is_shutdown():
             msg = DrivingTask()
             x_move = gamepad.get_one(gamepad.LS_XAXIS)
             y_move = gamepad.get_one(gamepad.LS_YAXIS)
             rot = gamepad.get_one(gamepad.RS_XAXIS)
 
-            if (last_x_move, last_y_move, last_rot) != (x_move, y_move, rot):
-                last_x_move, last_y_move, last_rot = x_move, y_move, rot
-                print "x:%.03f, y:%.03f, rot:%.03f" % (x_move, y_move, rot)
+            threshold = 0.04
+            if x_move < threshold and x_move > -threshold:
+                x_move = 0
+            if y_move < threshold and y_move > -threshold:
+                y_move = 0
+            if rot < threshold and rot > -threshold:
+                rot = 0
+
+            print "x:%.03f, y:%.03f, rot:%.03f" % (x_move, y_move, rot)
 
             msg.trans_x = x_move
             msg.trans_y = y_move
             msg.rot = rot
             self.pub.publish(msg)
             time.sleep(0.05)
-            print "send cmd"
 
 
 if __name__ == "__main__":
