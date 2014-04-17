9701bfe72dc5b59757221f35ce9b2c4103ea00f2
ROGR <dane.t.larsen@gmail.com>
2014-04-17 10:27:12 -0600

    updated

--- catkin_ws/src/xhab_rogr/src/nodes/rogr_topics.py
+++ catkin_ws/src/xhab_rogr/src/nodes/rogr_topics.py
@@ -7,18 +7,17 @@ roslib.load_manifest('xhab_rogr')
 from xhab_rogr.msg import *
 
 
-TASK_MESSAGES = [ArmCameraTask, ArmTask, BatteryTask, Camera1Task, Camera2Task, CameraTurretTask, DistanceTask, LiftDriverTask, 
-		NutrientPumpTask, NutrientTankTask, NutrientValveTask, PressureTask, WaterPumpTask, WaterTankTask, WaterValveTask, 
-		WheelDriverTask]
+TASK_MESSAGES = [ArmTask, BatteryTask, CameraTurretTask, DistanceTask, LiftDriverTask, NutrientPumpTask, NutrientTankTask,   
+		PressureTask, WaterPumpTask, WaterTankTask, WaterValveTask, NutrientValveTask, WheelDriverTask]
+		
+DATA_MESSAGES = [Data]
 
-DATA_MESSAGES = [Data, CameraData]
-
-TOPICS = ["armcamera", "arm", "battery", "camera1", "camera2", "distance", "liftdriver", "nutrientpump", "nutrienttank", "nutrientvalve", 
+TOPICS = ["arm", "battery", "distance", "liftdriver", "nutrientpump", "nutrienttank", "nutrientvalve", 
          "pressure", "waterpump", "watertank", "watervalve", "wheeldriver", "cameraturret"]  
 
 PROPERTIES = ["battery_level", "charging_status", "battery_full", "distance", "lift_speed", "nutrient_level", "pressure", "water_level", 
              "water_pump_on", "nutrient_pump_on", "shoulder1_angle", "shoulder2_angle", "elbow1_angle", "elbow2_angle", "wrist_angle", 
-             "endeffector_angle", "camera1_status", "camera2_status", "arm_camera_status"] 
+             "endeffector_angle", "pan", "tilt"] 
 
 def make_task_publishers(base_topic):
     tt = zip(TOPICS, TASK_MESSAGES)
