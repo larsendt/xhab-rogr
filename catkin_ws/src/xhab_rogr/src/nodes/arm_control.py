1d8ba8f41834efc50a30fceae6b5085e36a0dd7c
ROGR <dane.t.larsen@gmail.com>
2014-04-23 21:29:30 -0600

    working on arm config

--- catkin_ws/src/xhab_rogr/src/nodes/arm_control.py
+++ catkin_ws/src/xhab_rogr/src/nodes/arm_control.py
@@ -10,70 +10,52 @@ from findUSBDev import *
 
 PUB_DELAY = 15
 
-shoulder1_angle = 0
-shoulder2_angle = 0
-elbow1_angle = 0
-elbow2_agle = 0
-wrist_angle = 0
-endeffector_angle = 0
-
 class ArmControl(object):
     def __init__(self):
         print "ArmControl init"
+        self.initialized = False
         rospy.init_node("ArmControl")
-        subtopic = "/tasks" + "/rogr" + "/arm"
-        pubtopic = "/data" + "/rogr" + "/arm"
-        self.pub = rospy.Publisher(pubtopic, Data)
-        self.sub = rospy.Subscriber(subtopic, ArmTask, self.callback)       
-	self.callback(ArmTask())
-	
-	
+        subtopic = "/tasks/rogr/arm"
+        self.sub = rospy.Subscriber(subtopic, ArmTask, self.callback)
+
+        USBPaths = findUSBDev()
+
+        USB0 = USB2Dynamixel_Device(USBPaths[0])
+        USB1 = USB2Dynamixel_Device(USBPaths[1])
+
+        self.shoulder1 = Robotis_Servo(USB0,1)
+        self.shoulder2 = Robotis_Servo(USB0,2)
+        self.elbow1 = Robotis_Servo(USB1,3)
+        self.elbow2 = Robotis_Servo(USB1,4)
+        self.wrist = Robotis_Servo(USB1,5)
+        self.endeffector = Robotis_Servo(USB1,6)
+
+        self.initialized = True
+
+
     def callback(self, msg):
         print "callback"
-        USBPaths = findUSBDev()
-	
-	USB0 = USB2Dynamixel_Device(USBPaths[0])
-	USB1 = USB2Dynamixel_Device(USBPaths[1])
-        
-	shoulder1 = Robotis_Servo(USB0,1)
-	shoulder2 = Robotis_Servo(USB0,2)
-	elbow1 = Robotis_Servo(USB1,3)
-	elbow2 = Robotis_Servo(USB1,4)
-	wrist = Robotis_Servo(USB1,5)
-	endeffector = Robotis_Servo(USB1,6)
-        
-       	shoulder1.move_angle(shoulder1_angle)
-        shoulder2.move_angle(shoulder2_angle)
-        elbow1.move_angle(elbow1_angle)
-	elbow2.move_angle(elbow2_angle)
-	wrist.move_angle(wrist_angle)
-	endeffector.move_angle(endeffector_angle)
-	
+        if not self.initialized:
+            print "Not yet initialized! Takes about a minute..."
+            return
+
+        print msg.endeffector_angle
+        self.shoulder1.move_angle(math.radians(msg.shoulder1_angle), blocking = False)
+        self.shoulder2.move_angle(math.radians(msg.shoulder2_angle), blocking = False)
+        self.elbow1.move_angle(math.radians(msg.elbow1_angle), blocking = False)
+        self.elbow2.move_angle(math.radians(msg.elbow2_angle), blocking = False)
+        self.wrist.move_angle(math.radians(msg.wrist_angle), blocking = False)
+        self.endeffector.move_angle(math.radians(msg.endeffector_angle), blocking = False)
+
 
     def spin(self):
-        while not rospy.is_shutdown():
-            pubmsg = Data()
-	    pubmsg.property = "shoulder1 angle"
-            pubmsg.value = shoulder1_angle
-	    pubmsg.property = "shoulder2 angle"
-            pubmsg.value = shoulder2_angle
-	    pubmsg.property = "elbow1 angle"
-            pubmsg.value = elbow1_angle
-            pubmsg.property = "elbow2 angle"
-            pubmsg.value = elbow2_angle
-	    pubmsg.property = "wrist angle"
-            pubmsg.value = wrist_angle
-	    pubmsg.property = "endeffector angle"
-            pubmsg.value = endeffector_angle
-	    pubmsg.timestamp = rospy.Time.now() 
-            self.pub.publish(pubmsg)
-            time.sleep(PUB_DELAY)
-
+        print "ArmControl listening"
+        rospy.spin()
 
 if __name__ == "__main__":
     try:
         p = ArmControl()
-	p.spin()
-        
+        p.spin()
+
     except rospy.ROSInterruptException:
         pass
--- catkin_ws/src/xhab_rogr/src/nodes/servo_config.py
+++ catkin_ws/src/xhab_rogr/src/nodes/servo_config.py
@@ -3,35 +3,35 @@ import math
 # home position in encoder ticks for the servo.
 
 servo_param = {
-  
+
     1: {                        # Shoulder 1
-        'home_encoder': 201,
-        'max_ang': math.radians(360.),
-        'min_ang': math.radians(-10.),
-        'max_encoder' : 4095,
-        'max_speed' : math.radians(50),
-        'rad_per_enc' : math.radians(251.)/4095.  
-       },
-    2: {                        # Shoulder 2
-        'home_encoder': 140,
-        'max_ang': math.radians(360.),
-        'min_ang': math.radians(-10.),
+        'home_encoder': -1550,
+        'max_ang': math.radians(220.),
+        'min_ang': math.radians(90.),
         'max_encoder' : 4095,
         'max_speed' : math.radians(50),
         'rad_per_enc' : math.radians(251.)/4095.
-       }, 
+       },
+    2: {                        # Shoulder 2
+        'home_encoder': -200,
+        'max_ang': math.radians(220.),
+        'min_ang': math.radians(90.),
+        'max_encoder' : 4095,
+        'max_speed' : math.radians(50),
+        'rad_per_enc' : math.radians(251.)/4095.
+       },
     3: {                        # elbow1
         'home_encoder': 170,
-        'max_ang': math.radians(360.),
-        'min_ang': math.radians(-10.),
+        'max_ang': math.radians(300.),
+        'min_ang': math.radians(45.),
         'max_encoder' : 4095,
         'max_speed' : math.radians(50),
         'rad_per_enc' : math.radians(360.)/4095.
        },
     4: {                        # elbow2
-        'home_encoder': 274,
-        'max_ang': math.radians(360.),
-        'min_ang': math.radians(-10.),
+        'home_encoder': -550,
+        'max_ang': math.radians(280.),
+        'min_ang': math.radians(75.),
         'max_encoder' : 4095,
         'max_speed' : math.radians(50),
         'rad_per_enc' : math.radians(360.)/4095.
@@ -45,12 +45,12 @@ servo_param = {
         'rad_per_enc' : math.radians(360.)/4095.
        },
     6: {                        # End Effector
-        'home_encoder': 0,
+        #'home_encoder': -4095,
         'max_ang': math.radians(360.),
         'min_ang': math.radians(-360.),
-        'max_encoder' : 4095,
+        #'max_encoder' : 4095,
         'rad_per_enc' : math.radians(360.)/4095.
        }
-   
+
 }
 
